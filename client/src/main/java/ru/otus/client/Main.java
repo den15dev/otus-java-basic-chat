26ad62acac6002c4dd7ab43f61243b1791edc396
den15 <dendangler@gmail.com>
2026-03-07 00:22:46 +0300

    0.0.1 Create modules

---
+++ client/src/main/java/ru/otus/client/Main.java
@@ -0,0 +1,7 @@
+package ru.otus.client;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from Client!");
+    }
+}
---
+++ server/src/main/java/ru/otus/server/Main.java
@@ -0,0 +1,7 @@
+package ru.otus.server;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from Server!");
+    }
+}
